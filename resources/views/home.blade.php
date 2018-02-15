429ca5c140968b0e0bbaffc7f7661d1cb5c24ac5
Marcelo <marcelo@blitsoft.com.br>
2018-02-15 17:28:47 -0300

    usuarios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 
         <header class="main-header">
             <!-- Logo -->
-            <a href="../../index2.html" class="logo">
+            <a href="/home" class="logo">
                 <!-- mini logo for sidebar mini 50x50 pixels -->
                 <span class="logo-mini"><b>A</b>GD</span>
                 <!-- logo for regular state and mobile devices -->
@@ -24,88 +24,6 @@
 
                 <div class="navbar-custom-menu">
                     <ul class="nav navbar-nav">
-                        <!-- Messages: style can be found in dropdown.less-->
-                        {{--<li class="dropdown messages-menu">--}}
-                            {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown">--}}
-                                {{--<i class="fa fa-envelope-o"></i>--}}
-                                {{--<span class="label label-success">4</span>--}}
-                            {{--</a>--}}
-                            {{--<ul class="dropdown-menu">--}}
-                                {{--<li class="header">You have 4 messages</li>--}}
-                                {{--<li>--}}
-                                    {{--<!-- inner menu: contains the actual data -->--}}
-                                    {{--<ul class="menu">--}}
-                                        {{--<li><!-- start message -->--}}
-                                            {{--<a href="#">--}}
-                                                {{--<div class="pull-left">--}}
-                                                    {{--<img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">--}}
-                                                {{--</div>--}}
-                                                {{--<h4>--}}
-                                                    {{--Support Team--}}
-                                                    {{--<small><i class="fa fa-clock-o"></i> 5 mins</small>--}}
-                                                {{--</h4>--}}
-                                                {{--<p>Why not buy a new awesome theme?</p>--}}
-                                            {{--</a>--}}
-                                        {{--</li>--}}
-                                        {{--<!-- end message -->--}}
-                                    {{--</ul>--}}
-                                {{--</li>--}}
-                                {{--<li class="footer"><a href="#">See All Messages</a></li>--}}
-                            {{--</ul>--}}
-                        {{--</li>--}}
-                        <!-- Notifications: style can be found in dropdown.less -->
-                        {{--<li class="dropdown notifications-menu">--}}
-                            {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown">--}}
-                                {{--<i class="fa fa-bell-o"></i>--}}
-                                {{--<span class="label label-warning">10</span>--}}
-                            {{--</a>--}}
-                            {{--<ul class="dropdown-menu">--}}
-                                {{--<li class="header">You have 10 notifications</li>--}}
-                                {{--<li>--}}
-                                    {{--<!-- inner menu: contains the actual data -->--}}
-                                    {{--<ul class="menu">--}}
-                                        {{--<li>--}}
-                                            {{--<a href="#">--}}
-                                                {{--<i class="fa fa-users text-aqua"></i> 5 new members joined today--}}
-                                            {{--</a>--}}
-                                        {{--</li>--}}
-                                    {{--</ul>--}}
-                                {{--</li>--}}
-                                {{--<li class="footer"><a href="#">View all</a></li>--}}
-                            {{--</ul>--}}
-                        {{--</li>--}}
-                        <!-- Tasks: style can be found in dropdown.less -->
-                        {{--<li class="dropdown tasks-menu">--}}
-                            {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown">--}}
-                                {{--<i class="fa fa-flag-o"></i>--}}
-                                {{--<span class="label label-danger">9</span>--}}
-                            {{--</a>--}}
-                            {{--<ul class="dropdown-menu">--}}
-                                {{--<li class="header">You have 9 tasks</li>--}}
-                                {{--<li>--}}
-                                    {{--<!-- inner menu: contains the actual data -->--}}
-                                    {{--<ul class="menu">--}}
-                                        {{--<li><!-- Task item -->--}}
-                                            {{--<a href="#">--}}
-                                                {{--<h3>--}}
-                                                    {{--Design some buttons--}}
-                                                    {{--<small class="pull-right">20%</small>--}}
-                                                {{--</h3>--}}
-                                                {{--<div class="progress xs">--}}
-                                                    {{--<div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">--}}
-                                                        {{--<span class="sr-only">20% Complete</span>--}}
-                                                    {{--</div>--}}
-                                                {{--</div>--}}
-                                            {{--</a>--}}
-                                        {{--</li>--}}
-                                        {{--<!-- end task item -->--}}
-                                    {{--</ul>--}}
-                                {{--</li>--}}
-                                {{--<li class="footer">--}}
-                                    {{--<a href="#">View all tasks</a>--}}
-                                {{--</li>--}}
-                            {{--</ul>--}}
-                        {{--</li>--}}
                         <!-- User Account: style can be found in dropdown.less -->
                         <li class="dropdown user user-menu">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -122,22 +40,6 @@
                                         <small></small>
                                     </p>
                                 </li>
-                                <!-- Menu Body -->
-                                {{--<li class="user-body">--}}
-                                    {{--<div class="row">--}}
-                                        {{--<div class="col-xs-4 text-center">--}}
-                                            {{--<a href="#">Configurar</a>--}}
-                                        {{--</div>--}}
-                                        {{--<div class="col-xs-4 text-center">--}}
-                                            {{--<a href="#">Sales</a>--}}
-                                        {{--</div>--}}
-                                        {{--<div class="col-xs-4 text-center">--}}
-                                            {{--<a href="#">Friends</a>--}}
-                                        {{--</div>--}}
-                                    {{--</div>--}}
-                                    {{--<!-- /.row -->--}}
-                                {{--</li>--}}
-                                <!-- Menu Footer-->
                                 <li class="user-footer">
                                     <div class="pull-left">
                                         <a href="#" class="btn btn-default btn-flat">Profile</a>
@@ -148,10 +50,6 @@
                                 </li>
                             </ul>
                         </li>
-                        <!-- Control Sidebar Toggle Button -->
-                        {{--<li>--}}
-                            {{--<a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>--}}
-                        {{--</li>--}}
                     </ul>
                 </div>
             </nav>
@@ -190,26 +88,20 @@
                     <li class="">
                         <a href="/home">
                             <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-
                         </a>
-                        {{--<ul class="treeview-menu">--}}
-                            {{--<li><a href="../../index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>--}}
-                            {{--<li><a href="../../index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>--}}
-                        {{--</ul>--}}
                     </li>
-                    <li class="treeview active">
+                    <li class="treeview ">
                         <a href="#">
-                            <i class="fa fa-files-o"></i>
-                            <span>Layout Options</span>
+                            <i class="fa fa-gear"></i>
+                            <span>Configurações</span>
                             <span class="pull-right-container">
-                              <span class="label label-primary pull-right">4</span>
+                              <i class="fa fa-angle-left pull-right"></i>
                             </span>
                         </a>
                         <ul class="treeview-menu">
-                            <li><a href="../layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-                            <li><a href="../layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
-                            <li class="active"><a href="../layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
-                            <li><a href="collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
+                            <li><a href="/users"><i class="fa fa-circle-o"></i> Usuários</a></li>
+                            <li><a href="#"><i class="fa fa-circle-o"></i> Papéis</a></li>
+                            <li><a href="#"><i class="fa fa-circle-o"></i> Permissões</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -235,10 +127,7 @@
         <!-- /.content-wrapper -->
 
         <footer class="main-footer">
-            <div class="pull-right hidden-xs">
-                <b>Version</b> 2.4.0
-            </div>
-            <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+            <strong>Copyright &copy; 2014-2016 MGF.</strong> All rights
             reserved.
         </footer>
 
